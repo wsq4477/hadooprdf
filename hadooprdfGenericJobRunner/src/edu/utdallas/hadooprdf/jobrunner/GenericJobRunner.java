2b86f2609f0f679b3602c62e20f411f76cf5caf6
rockervaibhav <rockervaibhav@b85f50e6-92fd-b578-cf02-4bba7a10dc19>
2010-05-01 23:05:01 +0000

    Initial design

--- hadooprdfGenericJobRunner/src/edu/utdallas/hadooprdf/jobrunner/GenericJobRunner.java
+++ hadooprdfGenericJobRunner/src/edu/utdallas/hadooprdf/jobrunner/GenericJobRunner.java
@@ -1,6 +1,9 @@
 package edu.utdallas.hadooprdf.jobrunner;
 
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
 import java.util.StringTokenizer;
 
 import org.apache.hadoop.io.Text;
@@ -14,8 +17,26 @@ import org.apache.hadoop.mapreduce.Reducer;
  */
 public class GenericJobRunner 
 {
+	//TODO: How to get these parameters ?? Query plan ??
+	/** The joining variable: subject or object **/
 	private String joinVariable = "subject";
+	
+	/** The number of variables in every triple pattern **/
 	private int numOfVariables = 0;
+	
+	/** The input filename **/
+	private String filename = "";
+	
+	/** The map for prefixes added from the first job **/
+	private static final Map<String,String> hm;
+	static
+	{
+		hm = new HashMap<String,String>();
+		
+		//This comes from the triple pattern that contains the joining variable for the next job
+		hm.put( "M#", "" );
+		hm.put( "S#", "" );
+	}
 	
 	/**
 	 * The generic mapper class for a SPARQL query
@@ -28,19 +49,13 @@ public class GenericJobRunner
 		 * The setup method for this mapper
 		 * @param context - the context 
 		 */
-		protected void setup( Context context )
-		{
-			
-		}
+		protected void setup( Context context ) { }
 		
 		/**
 		 * The cleanup method for this mapper
 		 * @param context - the context
 		 */
-		protected void cleanup( Context context )
-		{
-			
-		}
+		protected void cleanup( Context context ) { }
 		
 		/**
 		 * The map method
@@ -53,10 +68,12 @@ public class GenericJobRunner
 			//Tokenize the value
 			StringTokenizer st = new StringTokenizer( value.toString(), "\t" ); 
 			
-			//First check if a key exists, if it does we are looking at a file
-			//Else this maybe a second job
+			//First check if the key is an input filename, if it is then do file processing based map
+			//Else this maybe a second job, do a prefix based map
 			String sPredicate = key.toString();
-			if( sPredicate != null )
+			
+			//TODO: Need to check if the key matches any input filename
+			if( sPredicate.equalsIgnoreCase( filename ) )
 			{
 				//Get the subject
 				String sSubject = st.nextToken();
@@ -95,7 +112,14 @@ public class GenericJobRunner
 			}
 			else
 			{
-				
+				while( st.hasMoreTokens() )
+				{
+					String token = st.nextToken();
+					if( hm.containsKey( token.substring( 0, 2 ) ) )
+					{
+						context.write( new Text( token.substring( 2 ) ), key );
+					}
+				}
 			}
 		}
 	}
@@ -131,9 +155,20 @@ public class GenericJobRunner
 		 * @param value - the input value
 		 * @param context - the context
 		 */
-		protected void reduce( Text key, Text value, Context context )
+		protected void reduce( Text key, Iterable<Text> value, Context context ) throws IOException, InterruptedException
 		{
-			
+            String sValue = "";
+            
+            //Iterate over all values for a particular key
+            Iterator<Text> iter = value.iterator();
+            while ( iter.hasNext() ) 
+            {
+                    sValue += iter.next().toString() + '\t';
+            }
+            
+            //TODO: How to find the order of results with the given query, may need rearranging of value here
+            //Write the result
+            context.write( key, new Text( sValue ) );		
 		}
 	}
 }
